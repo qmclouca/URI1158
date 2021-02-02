20106f9e7a388c64e4e6f9a443e02c67af437571
qmclouca <qmclouca@gmail.com>
2021-02-01 21:08:00 -0300

    Segunda versão. Aceita, versão final.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 void compute(int from, int oddsToPlus){
-    int sum = 0, count = 0;
+    int sum = 0;
 
     if (from%2 !=0){
-        for (int x = from; count<=oddsToPlus; x = x+2, count++) {
+        for (int x = from, count=1; count<=oddsToPlus; x = x+2, count++) {
             sum = sum+x;
         }
-        sum = sum + from;
     } else {
-        for (int x = from; count<=oddsToPlus; x = x+2, count++) {
+        for (int x = from+1, count=1; count<=oddsToPlus; x = x+2, count++) {
             sum = sum+x;
         }
     }
@@ -18,7 +17,7 @@ void compute(int from, int oddsToPlus){
 }
 
 int main() {
-    int inputs = 0, from[1000], oddsToPlus[1000], aux = 0, sum = 0, x = 0;
+    int inputs = 0, from[1000], oddsToPlus[1000];
     std::cin >> inputs;
     for (int x = 0; x<inputs;x++){
         from[x]=0;
